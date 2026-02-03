9579b32a41bf23393e241f723c4b0a2185aa9b84
Nikita <nkulik0v@protonmail.com>
2026-02-03 10:07:30 +0700

    Updates

---
+++ assets/index-Crbabb2q.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-G0VaQWva.js","./index-Bmy0SDzM.js"])))=>i.map(i=>d[i]);
+import{_ as r}from"./index-D1jJrNVF.js";import{r as t}from"./index-Bmy0SDzM.js";import"./three-DVIskRsv.js";const m=t("SplashScreen",{web:()=>r(()=>import("./web-G0VaQWva.js"),__vite__mapDeps([0,1]),import.meta.url).then(e=>new e.SplashScreenWeb)});export{m as SplashScreen};
